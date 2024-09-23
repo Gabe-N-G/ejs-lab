11497341abcb2f7be62dad48d0852f3ed6f26543
g g <gabedotgutierrez@gmail.com>
2024-09-23 15:53:09 -0400

    menu foreach done

--- server.js
+++ server.js
@@ -59,6 +59,12 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/menu', (req, res) => {
+    res.render('menu.ejs',{
+      menu: RESTAURANT.menu
+    });
+  });
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
